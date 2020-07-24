d0ee37ba4ac64cfd62593bf5ca886d04eb65e519
Munira Hunaid <51707912+muneerra@users.noreply.github.com>
2020-07-25 00:16:49 +0500

    files added

---
+++ script.js
@@ -0,0 +1,8 @@
+function night() {
+    let element = document.body;
+    element.classList.toggle("nightMode")
+}
+function day() {
+    let element2 = document.body;
+    element2.classList.toggle("dayMode")
+}
